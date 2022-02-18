d4d1f266f10f2b61d4976735d158e694a698842b
Usman Asif <usmanasif@users.noreply.github.com>
2022-02-18 09:53:20 -0800

    Fix inclusion syntax

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,9 @@
 class Device < ApplicationRecord
   belongs_to :player
 
-  validates :operating_system, :inclusion=> { in: ['ios','android'] }
+  OS_VERSIONS = ['ios', 'android']
+
+  validates :operating_system, inclusion: { in: OS_VERSIONS }
   validates :os_version, format: { with: /(\d+\.)(\d+\.)(\d+)/, message: 'Use the syntax: 1.0.0' }
   validates :locale, format: { with: /[a-z]{2}_[A-Z]{2}/, message: 'Use the syntax: fr_CA' }
 end

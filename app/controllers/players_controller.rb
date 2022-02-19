5ff1bbd8685c56b01998341e6ec5f6db522954dd
Usman Asif <usmanasif@users.noreply.github.com>
2022-02-19 01:12:00 -0800

    Fixed indentation

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,11 +10,11 @@ class PlayersController < BaseController
     players = if params[:offer_target_id].present?
                 offer_target = OfferTarget.find(params[:offer_target_id])
                 Player.includes(:device)
-                                .where('birthdate BETWEEN ? AND ?', offer_target.max_birtdate, offer_target.min_birtdate)
-                                .where(gender: offer_target.gender)
-                                .where(devices: { operating_system: offer_target.operating_system })
-                                .where(devices: { os_version: offer_target.minimum_os_version } )
-                                .where(devices: { locale: offer_target.locale })
+                      .where('birthdate BETWEEN ? AND ?', offer_target.max_birtdate, offer_target.min_birtdate)
+                      .where(gender: offer_target.gender)
+                      .where(devices: { operating_system: offer_target.operating_system })
+                      .where(devices: { os_version: offer_target.minimum_os_version } )
+                      .where(devices: { locale: offer_target.locale })
 
               else
                 Player.all
